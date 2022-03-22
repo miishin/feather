d9741bdc42e0cea839b0d0a5b3c3853e3013a3aa
miishin <miishing@gmail.com>
2022-03-22 14:21:01 -0400

    spacing issues fixed for gallery pages

--- pages/art.js
+++ pages/art.js
@@ -9,7 +9,7 @@ export default function Art(props) {
   return (
     <div className = "h-screen">
       <Header/>
-      <div className = "grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 xl:container xl:mx-auto items-center">
+      <div className = "grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 xl:container xl:mx-auto items-center flex-grow">
           <>
           { props["resources"].map(img =>         
             <div className = " w-full rounded py-1 md:px-1" key = { img.filename }>
--- pages/photography.js
+++ pages/photography.js
@@ -9,7 +9,7 @@ export default function Photography(props) {
   return (
     <div className = "flex flex-col min-h-screen">
       <Header/>
-      <div className = "grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 xl:container xl:mx-auto items-center">
+      <div className = "grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 xl:container xl:mx-auto items-center flex-grow">
           <>
           { props["resources"].map(img =>         
             <div className =" w-full rounded py-1 md:px-1" key = { img.filename }>
