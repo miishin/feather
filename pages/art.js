a07117142bf182ad6c31a4b2b85c345c50c1f779
miishin <miishing@gmail.com>
2022-03-22 14:05:53 -0400

    image alignment

--- pages/art.js
+++ pages/art.js
@@ -9,7 +9,7 @@ export default function Art(props) {
   return (
     <div className = "h-screen">
       <Header/>
-      <div className = "grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 xl:container xl:mx-auto">
+      <div className = "grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 xl:container xl:mx-auto items-center">
           <>
           { props["resources"].map(img =>         
             <div className = " w-full rounded py-1 md:px-1" key = { img.filename }>
