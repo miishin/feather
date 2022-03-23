90676b4f27ad10f0c2380dad215bac9f17c9e55a
miishin <miishing@gmail.com>
2022-03-22 22:48:44 -0400

    remove unused imports

--- pages/photography.js
+++ pages/photography.js
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import getImages from '../components/gallery'
