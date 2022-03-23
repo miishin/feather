7286140b1a55f1481bb8bac62ec60678f58886b4
miishin <miishing@gmail.com>
2022-03-22 22:52:31 -0400

    rename file, and simplify argument

--- components/cloudinary.js
+++ components/cloudinary.js
@@ -1,16 +1,12 @@
 // https://spacejelly.dev/posts/how-to-list-display-cloudinary-image-resources-in-a-gallery-with-next-js-react/
-export default async function getImages(options = {}) {
-    const params = {
-      ...options
-    }
+
+export default async function getImages(params) {
     const paramString = Object.keys(params).map(key => `${key}=${encodeURIComponent(params[key])}`).join('&');
-  
     const results = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUD_NAME}/resources/search?${paramString}`, {
       headers: {
         Authorization: `Basic ${Buffer.from(process.env.API_KEY + ':' + process.env.API_SECRET).toString('base64')}`
       }
     }).then(r => r.json());
-    
     const { resources } = results
     return resources;
   }
