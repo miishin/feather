2c85703a8c3e98a1f64aa0093788587be76f295e
miishin <miishing@gmail.com>
2022-03-22 22:56:28 -0400

    add l/r padding for images

--- pages/art.js
+++ pages/art.js
@@ -7,7 +7,7 @@ export default function Art(props) {
   return (
     <div className = "h-screen">
       <Header/>
-      <div className = "grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 xl:container xl:mx-auto items-center flex-grow">
+      <div className = "px-5 grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 xl:container xl:mx-auto items-center flex-grow">
           <>
           { props["resources"].map(img =>         
             <div className = " w-full rounded py-1 md:px-1" key = { img.filename }>
